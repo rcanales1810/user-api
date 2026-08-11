4d26924b6d3c678a23735e2103457f51603796aa
rcanales1810 <r.canales1810@gmail.com>
2026-08-11 15:20:07 -0500

    Remove try/catch due to Express version 5 includes asyncHandling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,171 +1,142 @@
 const userService = require("../services/user.service");
 const { isValidEmail } = require("../utils/validators");
 
-const getUsers = async (req, res, next) => {
-    try {
-        const { includeInactive } = req.query;
-        const shouldIncludeInactive = includeInactive === "true";
+const getUsers = async (req, res) => {
+    const { includeInactive } = req.query;
+    const shouldIncludeInactive = includeInactive === "true";
 
-        let users;
+    let users;
 
-        if (shouldIncludeInactive) {
-            users = await userService.getAllUsers();
-        } else {
-            users = await userService.getActiveUsers();
-        }
-
-        return res.status(200).json(users);
-    } catch {
-        next(error);
+    if (shouldIncludeInactive) {
+        users = await userService.getAllUsers();
+    } else {
+        users = await userService.getActiveUsers();
     }
+
+    return res.status(200).json(users);
 };
 
-const createNewUser = async (req, res, next) => {
+const createNewUser = async (req, res) => {
 
-    try {
-        const { name, email } = req.body;
+    const { name, email } = req.body;
 
-        //validación de los datos ingresados
-        //"si name o email son falsos", o sea, si uno o el otro están vacios
-        if (!name || !email) {
-            //Los códigos 4xx significan errores del cliente, en síntesis, el cliente hizo algo mal. En este caso
-            //no ha completado campos obligatorios. Sería bueno hacer que en la tabla en la BD estos campos sean NOT NULL
-            return res.status(400).json({
-                message: "Ambos campos son obligatorios"
-            });
-        }
-
-        //verificar que el formato del correo sea correcto 
-        if (!isValidEmail(email)) {
-            return res.status(400).json({
-                message: "El correo no tiene el formato correcto: user@mail.com"
-            });
-        }
-
-        const user = await userService.createNewUser(name, email);
-
-        res.status(201).json({
-            message: "Usuario creado correctamente",
-            user
+    //validación de los datos ingresados
+    //"si name o email son falsos", o sea, si uno o el otro están vacios
+    if (!name || !email) {
+        //Los códigos 4xx significan errores del cliente, en síntesis, el cliente hizo algo mal. En este caso
+        //no ha completado campos obligatorios. Sería bueno hacer que en la tabla en la BD estos campos sean NOT NULL
+        return res.status(400).json({
+            message: "Ambos campos son obligatorios"
         });
+    }
 
-    } catch (error) {
-        next(error);
-    };
+    //verificar que el formato del correo sea correcto 
+    if (!isValidEmail(email)) {
+        return res.status(400).json({
+            message: "El correo no tiene el formato correcto: user@mail.com"
+        });
+    }
+
+    const user = await userService.createNewUser(name, email);
+
+    res.status(201).json({
+        message: "Usuario creado correctamente",
+        user
+    });
 };
 
 const getUserById = async (req, res, next) => {
-    try {
-        const user = await userService.getUserById(req.userId);
-        //El 404 va FUERA DEL CATCH porque no es un error de conexión ni de sintaxis ni de la BD. TODO está bien, pero no se encontró el id buscado
-        if (!user) {
-            return res.status(404).json({
-                message: "Usuario no encontrado"
-            });
-        }
-
-        res.status(200).json({
-            user
+    const user = await userService.getUserById(req.userId);
+    //El 404 va FUERA DEL CATCH porque no es un error de conexión ni de sintaxis ni de la BD. TODO está bien, pero no se encontró el id buscado
+    if (!user) {
+        return res.status(404).json({
+            message: "Usuario no encontrado"
         });
-    } catch (error) {
-        next(error);
     }
+
+    res.status(200).json({
+        user
+    });
 };
 
 const updateUser = async (req, res, next) => {
-    try {
-        //Obtener el id, user y email
-        const { name, email } = req.body;
+    //Obtener el id, user y email
+    const { name, email } = req.body;
 
-        if (!name || !email) {
-            return res.status(400).json({
-                message: "Ambos campos son obligatorios"
-            });
-        }
-
-        //despues, validar que los datos a actualizar sean correctos
-        if (!isValidEmail(email)) {
-            return res.status(400).json({
-                message: "El correo no tiene el formato correcto: user@mail.com"
-            });
-        }
-
-        //verificar si existe el usuario a actualizar
-        const user = await userService.getUserById(req.userId);
-
-        if (!user) {
-            return res.status(404).json({
-                message: "Usuario no encontrado"
-            });
-        }
-
-        //Llamar al servicio para actualizar
-        const updatedUser = await userService.updateUser(name, email, req.userId);
-
-        res.status(200).json({
-            updatedUser
+    if (!name || !email) {
+        return res.status(400).json({
+            message: "Ambos campos son obligatorios"
         });
-
-    } catch (error) {
-        next(error);
     }
 
+    //despues, validar que los datos a actualizar sean correctos
+    if (!isValidEmail(email)) {
+        return res.status(400).json({
+            message: "El correo no tiene el formato correcto: user@mail.com"
+        });
+    }
+
+    //verificar si existe el usuario a actualizar
+    const user = await userService.getUserById(req.userId);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "Usuario no encontrado"
+        });
+    }
+
+    //Llamar al servicio para actualizar
+    const updatedUser = await userService.updateUser(name, email, req.userId);
+
+    res.status(200).json({
+        updatedUser
+    });
 };
 
 const deleteUser = async (req, res, next) => {
-    try {
-        const user = await userService.getUserById(req.userId);
+    const user = await userService.getUserById(req.userId);
 
-        if (!user) {
-            return res.status(404).json({
-                message: "Usuario no encontrado"
-            });
-        }
-
-        const deletedUser = await userService.deleteUser(req.userId);
-
-        res.status(200).json({
-            deletedUser
+    if (!user) {
+        return res.status(404).json({
+            message: "Usuario no encontrado"
         });
-
-    } catch (error) {
-        next(error);
     }
+
+    const deletedUser = await userService.deleteUser(req.userId);
+
+    res.status(200).json({
+        deletedUser
+    });
 };
 
 const patchUser = async (req, res, next) => {
-    try {
-        const { name, email } = req.body;
+    const { name, email } = req.body;
 
-        if (name === undefined && email === undefined) {
-            return res.status(400).json({
-                message: "Debes proporcionar al menos un campo para actualizar"
-            });
-        }
-
-        if (email !== undefined && !isValidEmail(email)) {
-            return res.status(400).json({
-                message: "El correo no tiene el formato correcto: user@mail.com"
-            });
-        }
-
-        const user = await userService.getUserById(req.userId);
-
-        if (!user) {
-            return res.status(404).json({
-                message: "Usuario no encontrado"
-            });
-        }
-
-        const patchedUser = await userService.patchUser(name, email, req.userId);
-
-        res.status(200).json({
-            patchedUser
+    if (name === undefined && email === undefined) {
+        return res.status(400).json({
+            message: "Debes proporcionar al menos un campo para actualizar"
         });
-
-    } catch (error) {
-        next(error);
     }
+
+    if (email !== undefined && !isValidEmail(email)) {
+        return res.status(400).json({
+            message: "El correo no tiene el formato correcto: user@mail.com"
+        });
+    }
+
+    const user = await userService.getUserById(req.userId);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "Usuario no encontrado"
+        });
+    }
+
+    const patchedUser = await userService.patchUser(name, email, req.userId);
+
+    res.status(200).json({
+        patchedUser
+    });
 };
 
 module.exports = {
